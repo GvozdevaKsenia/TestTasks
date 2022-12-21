4312ecbbe9089c6bce26ea5518475e02148c7e7e
GvozdevaKsenia <gvozdeva.ksenia.m@gmail.com>
2022-12-21 16:37:46 +0300

    done2

---
+++ Task1/Gvozdeva_TestTask_Innowise.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*Дядя Сэм решил оптимизировать свою пасеку и сократить количество ульев,
+объединив некоторые из них.Был возведен улий - терем на 2023 жителя.
+Дядя Сэм просит помочь ему найти подходящую(первую) пару ульев,
+суммарная вместимость которых составит 2023 пчелы.*/
+
+
+class HiveofSam {
+private:
+	string filename = "Input task1.txt";
+public:
+	HiveofSam(string _filename) {
+		this->filename = filename;
+	};
+
+	void FileFunction() {
+
+		vector<int> vec;
+		int numbersofBees;
+
+		ifstream fileInput;
+		fileInput.open(filename);
+
+		if (fileInput.is_open()) 
+		{
+			while (fileInput >> numbersofBees) {
+				vec.push_back(numbersofBees);
+			}
+			for (int i = 0; i < vec.size(); i++) {
+				for (int j = 1; j < vec.size() - 1; j++) {
+					int result = vec[i] + vec[j];
+					if (result == 2023 ) {
+						cout <<vec[i]<< " - " << vec[j] << endl;
+						i = vec.size();
+						break;
+					}	
+				}
+			}
+		}
+		else 
+		{
+			cout << "Ошибка открытия файла!";
+		}
+		fileInput.close();
+	}
+    
+};
+
+int main()
+{
+	cout << "Uncle Sam! I can help you!"<<endl;
+	string filename = "Input task1.txt";
+	HiveofSam hive = HiveofSam(filename);
+	hive.FileFunction();
+
+}
---
+++ Task2/Task2_Gvozdeva_Innowise.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+class Array {
+private:
+    int size1;
+    int size2;
+public:
+
+    void Function() 
+    {
+        cout << "Enter the size of array1: " << endl;
+        cin >> size1;
+
+        vector<int> array1;
+        for (int i = 0; i < size1; i++)
+        {
+            array1.push_back(rand() % 100);
+            cout <<array1[i];
+            cout << endl;
+        }
+
+        cout << "Enter the size of array2: " << endl;
+        cin >> size2;
+       
+        vector<int> array2;
+        for (int i = 0; i < size2; i++)
+        {
+            array2.push_back(rand() % 100);
+            cout << array2[i];
+            cout << endl;
+        }
+        
+        vector<int> result;
+        result.insert(result.end(), array1.begin(), array1.end());
+        result.insert(result.end(), array2.begin(), array2.end());
+      
+        sort(result.begin(), result.end());
+        
+        cout << "Sorted result array: ";
+        for (auto i = result.begin(); i < result.end(); i++)
+        {
+            cout << " " << *i;
+        }
+    }
+};
+
+int main()
+{
+    Array array = Array();
+    array.Function();
+    
+    return 0;
+}
